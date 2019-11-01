33971d43ca8f32d7bf851d6de6070e59dda2c150
Johanna Paavola <johanna.paavola@siili.com>
2019-11-01 12:11:19 +0200

    adds the background picture to react app

---
+++ hedgemax/src/components/Calendar/Calendar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import background from "../../resources/background.png";
+
+const Calendar = props => {
+  return <img src={background} height="800px" width="1000px" />;
+};
+
+export default Calendar;
