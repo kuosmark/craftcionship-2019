4b11ab2d43cad83132efc9e4c9ea78c3838bc5f7
Johanna Paavola <johanna.paavola@siili.com>
2019-11-01 16:13:54 +0200

    update user mutation

--- hedgemax/src/App.js
+++ hedgemax/src/App.js
@@ -64,9 +64,7 @@ function App() {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossorigin="anonymous"
       />
-      <ApolloProvider client={client}>
-        <Calendar />
-      </ApolloProvider>
+      <Calendar />
     </div>
   );
 }
