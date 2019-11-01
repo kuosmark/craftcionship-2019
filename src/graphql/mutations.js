246110ace188ee4ebafe5508d8f0b754b78ab17d
Henna Kalliokoski <henna.kalliokoski@siili.com>
2019-11-01 10:28:47 +0200

    front folder and graphql api

---
+++ src/graphql/mutations.js
@@ -0,0 +1,51 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const createUser = `mutation CreateUser($input: CreateUserInput!) {
+  createUser(input: $input) {
+    id
+    date
+    name
+  }
+}
+`;
+export const updateUser = `mutation UpdateUser($input: UpdateUserInput!) {
+  updateUser(input: $input) {
+    id
+    date
+    name
+  }
+}
+`;
+export const deleteUser = `mutation DeleteUser($input: DeleteUserInput!) {
+  deleteUser(input: $input) {
+    id
+    date
+    name
+  }
+}
+`;
+export const createMeme = `mutation CreateMeme($input: CreateMemeInput!) {
+  createMeme(input: $input) {
+    id
+    url
+    name
+  }
+}
+`;
+export const updateMeme = `mutation UpdateMeme($input: UpdateMemeInput!) {
+  updateMeme(input: $input) {
+    id
+    url
+    name
+  }
+}
+`;
+export const deleteMeme = `mutation DeleteMeme($input: DeleteMemeInput!) {
+  deleteMeme(input: $input) {
+    id
+    url
+    name
+  }
+}
+`;
