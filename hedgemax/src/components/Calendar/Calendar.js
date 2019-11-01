7f55d8a7fb5356e4bb8ac3c337de340cbd7303fd
Johanna Paavola <johanna.paavola@siili.com>
2019-11-01 13:04:55 +0200

    first door

--- hedgemax/src/components/Calendar/Calendar.js
+++ hedgemax/src/components/Calendar/Calendar.js
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import background from "../../resources/background.png";
+import styles from "./Calendar.module.css";
 
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalFooter from "react-bootstrap/ModalFooter";
+import Modal from "react-bootstrap/Modal";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 const Calendar = props => {
-  return <img src={background} height="800px" width="1000px" />;
+  const [smShow, setSmShow] = useState(false);
+  return (
+    <div className={styles.calendar}>
+      <Modal
+        size="lg"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title
+            id="example-modal-sizes-title-sm"
+            style={{ width: "100px !important" }}
+          >
+            Door 24
+          </Modal.Title>
+          <InputGroup className="mb-3" style={{ width: "200px !important" }}>
+            <FormControl
+              placeholder="Name"
+              aria-label="Name"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+        </Modal.Header>
+        <Modal.Body>...</Modal.Body>
+      </Modal>
+
+      <img className={styles.calendar} src={background} />
+      <Door
+        number={"24"}
+        x={470}
+        y={600}
+        width={280}
+        height={200}
+        setSmShow={setSmShow}
+      />
+    </div>
+  );
+};
+
+const Door = props => {
+  const { number, height, width, x, y } = props;
+  return (
+    <div
+      className={styles.door}
+      onClick={() => props.setSmShow(true)}
+      style={{
+        width: width,
+        height: height,
+        left: x,
+        bottom: y,
+        border: "2px solid blue"
+      }}
+    >
+      {number}
+    </div>
+  );
 };
 
 export default Calendar;
