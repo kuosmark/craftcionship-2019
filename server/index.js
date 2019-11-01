cc5e30acae8eeced385b162cb4497cc0667b194c
Markus Kuosmanen <markus.kuosmanen@siili.com>
2019-11-01 14:27:09 +0200

    adds christmas lottery logic

--- server/index.js
+++ server/index.js
@@ -1,20 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const lottery = require('./lottery')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-require("dotenv").config();
+const app = express()
 
-const app = express();
-const port = 3001;
+app.use(bodyParser.json())
 
-app.use(bodyParser.json());
-
-app.get("/*", (req, res) => {
-  res.send("Running");
-});
-app.post("/", (req, res) => {
-  const body = req.body;
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}!`);
-});
+lottery.getWinner()
--- server/queries.js
+++ server/queries.js
@@ -1,14 +1,26 @@
-// Returns all keywords from database.
-const listKeywords = () => {
-  return `query listUsers {
-      listKeywords (limit: 10000) {
-        items {
-          id
-        }
+const getContestants = time => {
+  return `query getcontestants {
+    listUsers(limit: 700, filter: {date: {gt: ${time}}}) {
+      items {
+        id
+        name
+        date
+        winner
       }
-    }`;
-};
+    }
+  }`
+}
+
+const postNewWinner = (id, time) => {
+  return `mutation updateUser {
+    updateUser(input: {id: "${id}", winner: ${time}})
+    {id
+    name}
+  }
+  `
+}
 
 module.exports = {
-  listUSers
-};
+  getContestants,
+  postNewWinner
+}
